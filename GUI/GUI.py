e6cdd891bd4ecf070fd5a57306815cbe3a4dc3c7
KristianHyttel <khpe16@student.aau.dk>
2019-10-08 11:37:09 +0200

    Can make label say Hallo

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -2,12 +2,21 @@ from PyQt5 import uic, QtWidgets, QtCore
 import sys
 
 class Ui(QtWidgets.QMainWindow):
+
     def __init__(self):
+
         super(Ui,self).__init__()
+
         uic.loadUi("TestGui.ui",self)
+
+        self.printBtn.clicked.connect(self.printmsg)
         self.show()
+
+    def printmsg(self):
+        self.outputLbl.setText("Hallo World")
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = Ui()
     sys.exit(app.exec_())
+
