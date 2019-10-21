a5e9b70f3563758fadc0c5246d79ad933ee3aa96
KristianHyttel <khpe16@student.aau.dk>
2019-10-21 14:17:55 +0200

    first gui implementation

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,22 +1,23 @@
-from PyQt5 import uic, QtWidgets, QtCore
+import os
 import sys
+import threading
 
-class Ui(QtWidgets.QMainWindow):
-
-    def __init__(self):
-
-        super(Ui,self).__init__()
-
-        uic.loadUi("TestGui.ui",self)
-
-        self.printBtn.clicked.connect(self.printmsg)
-        self.show()
-
-    def printmsg(self):
-        self.outputLbl.setText("Hallo World")
+from PyQt5 import QtCore, QtWebEngineWidgets, QtWidgets, uic
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = Ui()
-    sys.exit(app.exec_())
+    class MyThread(threading.Thread):
+        def run(self):
+            os.system("voila mapselect.ipynb --no-browser")
+            pass
 
+    thread = MyThread()
+    thread.daemon = True
+    thread.start()
+
+    app = QtWidgets.QApplication(sys.argv)
+    path_ui = os.path.join(os.path.dirname(__file__), "Gui.ui")
+    window = uic.loadUi(path_ui)
+    window.mapWgt.setZoomFactor(1)
+    window.mapWgt.load(QtCore.QUrl('http://localhost:8866'))
+    window.show()
+    sys.exit(app.exec_())
