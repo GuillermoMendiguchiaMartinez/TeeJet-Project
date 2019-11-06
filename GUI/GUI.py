331b3e2498307422c79a7b789e83c39cddfbac59
bedab <bberner19@student.aau.dk>
2019-11-06 13:51:26 +0100

    images and masks moved to window

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -12,40 +12,47 @@ from PyQt5 import QtCore, QtWebEngineWidgets, QtWidgets, uic
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-        #start voila server
+        # start voila server
         self.thread = MyThread()
         self.thread.daemon = True
         self.thread.start()
         # load UI design and show the UI
         uic.loadUi('GUI.ui', self)
+
+        self.image = np.zeros((10, 10, 3)).astype('uint8')
+        self.mask = np.zeros((10, 10)).astype('uint8')
+        self.mask_downscaled = np.zeros((10, 10)).astype('uint8')
+        self.image_downscaled = self.image
+
         self.show()
 
         self.SegmentationViewer.onMouseMoved.connect(self.process_mouse_input)
+        self.SegmentationViewer.onWidgetResized.connect(self.process_viewer_resize)
 
         self.SegmentationViewer.onMousePressed.connect(self.process_mouse_input)
         self.threshold_slider.valueChanged.connect(self.process_threshold_slider)
-    def process_mouse_input(self):
-        x=self.SegmentationViewer.position.x()
-        y=self.SegmentationViewer.position.y()
-        self.x_label.setText('x: %d'%(x))
-        self.y_label.setText('y: %d' % (y))
-        if y<self.SegmentationViewer.image_org.shape[0] and x<self.SegmentationViewer.image_org.shape[1]:
-            self.color=self.SegmentationViewer.image_org[int(math.floor(y)),int(math.floor(x))]
 
-            self.color_label.setText('color: '+(str(self.color)))
+    def process_mouse_input(self):
+        x = self.SegmentationViewer.position.x()
+        y = self.SegmentationViewer.position.y()
+        self.x_label.setText('x: %d' % (x))
+        self.y_label.setText('y: %d' % (y))
+        if y < self.image.shape[0] and x < self.image.shape[1]:
+            self.color = self.image[int(math.floor(y)), int(math.floor(x))]
+
+            self.color_label.setText('color: ' + (str(self.color)))
 
             if self.SegmentationViewer.buttons == QtCore.Qt.LeftButton:
-                self.segmentation_color=self.SegmentationViewer.image_org[int(math.floor(y)),int(math.floor(x))]
-                self.chosen_color_label.setText('chosen color: '+(str(self.segmentation_color)))
-
-                self.SegmentationViewer.mask_downscaled=segment(self.SegmentationViewer.image,self.segmentation_color,self.threshold_slider.value())
-                self.SegmentationViewer.show_image()
+                self.segmentation_color = self.image[int(math.floor(y)), int(math.floor(x))]
+                self.chosen_color_label.setText('chosen color: ' + (str(self.segmentation_color)))
+                self.mask_downscaled = segment(self.image_downscaled, self.segmentation_color,
+                                               self.threshold_slider.value())
+                self.SegmentationViewer.show_image(self.image_downscaled, self.mask_downscaled, self.image.shape)
                 self.threshold_slider.setEnabled(True)
-    def process_threshold_slider(self):
-        self.SegmentationViewer.mask_downscaled = segment(self.SegmentationViewer.image, self.segmentation_color,
-                                                          self.threshold_slider.value())
-        self.SegmentationViewer.show_image()
 
+    def process_threshold_slider(self):
+        self.mask_downscaled = segment(self.image_downscaled, self.segmentation_color, self.threshold_slider.value())
+        self.SegmentationViewer.show_image(self.image_downscaled, self.mask_downscaled, self.image.shape)
 
     def closeEvent(self, event):
         # do stuff
@@ -54,24 +61,35 @@ class Ui(QtWidgets.QMainWindow):
         print(self.thread.is_alive())
         event.accept()  # let the window close
 
+    def load_image(self, image_path, max_dimension=2000):
+        self.image = cv2.imread(image_path)
+        max_dim_img = max(self.image.shape[0], self.image.shape[1])
+        if max_dim_img > max_dimension:
+            scaling_factor = max_dimension / max_dim_img
+            self.image_downscaled = cv2.resize(self.image, (
+                int(round(self.image.shape[1] * scaling_factor)), int(round(self.image.shape[0] * scaling_factor))))
+        else:
+            self.image_downscaled = self.image
+        self.mask = np.zeros((self.image.shape[0], self.image.shape[1]))
+        self.mask_downscaled = np.zeros((self.image_downscaled.shape[0], self.image_downscaled.shape[1]))
+
+    def process_viewer_resize(self):
+        self.SegmentationViewer.show_image(self.image_downscaled, self.mask_downscaled, self.image.shape)
 
 
 class MyThread(threading.Thread):
     def run(self):
-        self.process = subprocess.Popen(['voila', 'mapselect.ipynb', '--port=8866','--no-browser'])
-
+        self.process = subprocess.Popen(['voila', 'mapselect.ipynb', '--port=8866', '--no-browser'])
 
 
 if __name__ == '__main__':
-
-
     app = QtWidgets.QApplication(sys.argv)
     path_ui = os.path.join(os.path.dirname(__file__), "Gui.ui")
     window = Ui()
     window.mapWgt.setZoomFactor(1)
     window.mapWgt.load(QtCore.QUrl('http://localhost:8866'))
 
-    window.SegmentationViewer.load_image(r'C:\Users\krist\Desktop\pics\combinedblurlevels.png')
-    window.SegmentationViewer.show_image()
+    window.load_image(r'C:\Users\bedab\OneDrive\AAU\TeeJet-Project\res_image.jpg')
+    window.SegmentationViewer.show_image(window.image_downscaled, window.mask_downscaled, window.image.shape)
     window.show()
     sys.exit(app.exec_())
--- GUI/custom_widgets.py
+++ GUI/custom_widgets.py
@@ -6,15 +6,11 @@ import numpy as np
 class View(QtWidgets.QLabel):
     onMouseMoved = QtCore.pyqtSignal()
     onMousePressed = QtCore.pyqtSignal()
-    resized = QtCore.pyqtSignal()
+    onWidgetResized = QtCore.pyqtSignal()
     scaling_factor: float
 
     def __init__(self, parent=None):
-        self.image=np.zeros((10,10,3)).astype('uint8')
-        #self.image=cv2.imread(r'C:\Users\bedab\OneDrive\AAU\TeeJet-Project\Stitching\demonstration pictures\center picture 02\res_image.jpg')
-        self.mask=np.zeros((10,10)).astype('uint8')
-        self.mask_downscaled=np.zeros((10,10)).astype('uint8')
-        self.image_org=self.image
+
         self.scaling_factor=1
         QtWidgets.QLabel.__init__(self, parent)
 
@@ -27,39 +23,26 @@ class View(QtWidgets.QLabel):
         self.onMouseMoved.emit()
 
     def resizeEvent(self, event):
-        self.show_image()
-        self.resized.emit()
+        self.onWidgetResized.emit()
 
     def mousePressEvent(self, e: QtGui.QMouseEvent) -> None:
         self.buttons = e.buttons()
         self.onMousePressed.emit()
 
 
-    def load_image(self, image_path,max_dimension=2000):
-        self.image_org=cv2.imread(image_path)
-        max_dim_img=max(self.image_org.shape[0],self.image_org.shape[1])
-        if max_dim_img>max_dimension:
-            scaling_factor=max_dimension/max_dim_img
-            self.image=cv2.resize(self.image_org,(int(round(self.image_org.shape[1]*scaling_factor)),int(round(self.image_org.shape[0]*scaling_factor))))
-        else:
-            self.image=self.image_org
-        self.mask = np.zeros((self.image_org.shape[0],self.image_org.shape[1]))
-        self.mask_downscaled = np.zeros((self.image.shape[0], self.image.shape[1]))
-        self.setMouseTracking(True)
 
 
+    def show_image(self,image_downscaled,mask,image_org_shape):
+        masked_img=image_downscaled.copy()
+        masked_img[mask>0]=(0,0,255)
+        masked_img = cv2.addWeighted(masked_img, 0.4, image_downscaled, 0.6, 0)
+        image_converted=cv2.cvtColor(masked_img,cv2.COLOR_RGB2BGR)
 
-    def show_image(self):
-        masked_img=self.image.copy()
-        masked_img[self.mask_downscaled>0]=(255,0,0)
-        image_org = cv2.addWeighted(masked_img, 0.4, self.image, 0.6, 0)
-        image_converted=cv2.cvtColor(image_org,cv2.COLOR_RGB2BGR)
-
-        image_converted = QtGui.QImage(image_converted, image_org.shape[1], image_org.shape[0], image_org.shape[1] * 3,
+        image_converted = QtGui.QImage(image_converted, masked_img.shape[1], masked_img.shape[0], masked_img.shape[1] * 3,
                                        QtGui.QImage.Format_RGB888)
         image_converted = QtGui.QPixmap(image_converted)
         image_converted=image_converted.scaled(self.size(),QtCore.Qt.KeepAspectRatio)
 
-        self.scaling_factor = self.image_org.shape[0] / (image_converted.height())
+        self.scaling_factor = image_org_shape[0] / (image_converted.height())
         print((image_converted.height(),image_converted.width()))
         self.setPixmap(image_converted)
