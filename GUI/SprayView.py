13f017fdaccb7fa95da2d759a785b624b622cbc4
KristianHyttel <khpe16@student.aau.dk>
2019-11-19 15:21:22 +0100

    Delete SprayView.py

--- GUI/SprayView.py
+++
@@ -1,8 +0,0 @@
-from PyQt5 import QtWidgets, QtGui, QtCore
-import cv2
-import numpy as np
-
-
-class SprayView(QtWidgets.QLabel):
-    def __init__(self, parent=None):
-        QtWidgets.QLabel.__init__(self, parent)
